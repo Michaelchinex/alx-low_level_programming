f5fcbed12c90e3a4c499da87951bba76ec19cf87
Michaelchinex <okohagchidinma@gmail.com>
2022-04-10 13:13:06 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float f;
 printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a int: %d byte(s)\n",  (unsigned long)sizeof(a));
 printf("Size of a long int: %d byte(s)\n",  (unsigned long)sizeof(b));
-printf("Size of a long long: %d byte(s)\n",  (unsigned long)sizeof(c));
+printf("Size of a long long int: %d byte(s)\n",  (unsigned long long int)sizeof(c));
 printf("Size of a float: %d byte(s)\n",  (unsigned long)sizeof(f));
 return (0);
 } 
