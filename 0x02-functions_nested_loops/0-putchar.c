d539a10b8c55f870efa7c4d6689dbe9d2640da94
Michaelchinex <okohagchidinma@gmail.com>
2022-04-12 21:23:51 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * main - enry block
+ * main - entry block
  * @void: no argument
  * Return: 0
  **/
 int main(void)
+{	
+char ermias[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
 {
-	char ermiast[] = "_putchar";
-	int c;
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(ermiast[]);
-	}
-	_putchar('\n');
-	return (0);
+_putchar('ermias[]');
+}
+_putchar('\n');
+return (0);
 }
