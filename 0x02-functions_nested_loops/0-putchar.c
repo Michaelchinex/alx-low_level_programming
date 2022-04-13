dffa2ee5e60521affeff9007d055935080925f79
Michaelchinex <okohagchidinma@gmail.com>
2022-04-13 10:32:34 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  * Return: 0
  **/
 int main(void)
-{	
+}
 char erimas[] = "_putchar";
 int c;
 for (c = 0; c < 8; c++)
 {
-_putchar( -erimas[c]);
+_putchar(-erimas[c]);
 }
 _putchar('\n');
 return (0);
