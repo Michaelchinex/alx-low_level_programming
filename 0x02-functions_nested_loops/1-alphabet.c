ecc5c74cef49a779596825c95dd75925889db0f4
Michaelchinex <okohagchidinma@gmail.com>
2022-04-19 22:19:37 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,13 @@
 /**
 * main - Entry point
 */
+int main(void)
+{
 void print_alphabet(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 putchar('\n');
+}
 }
