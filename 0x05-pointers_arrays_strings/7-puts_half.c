b11edaa607c1d56ade54da2a1ce5d73e05ff69ea
Michaelchinex <okohagchidinma@gmail.com>
2022-04-21 13:57:56 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,18 +6,14 @@
 */
 void puts_half(char *str)
 {
-int j;
-int i;
-while (str[i] != '\0')
-i++;
-if (i % 2 == 0)
-j = i / 2;
-else
-j = (i + 1) / 2;
-while (j < i)
-{
-_putchar(str[j]);
-j++;
-}
+int j, i, len;
+len = 0;
+for (i = 0; str[i] != '\0'; i++)
+len++;
+n = (len / 2);
+if ((len % 2) == 1)
+n = ((len + 1) / 2);
+for (i = n; str[i] != '\0'; i++)
+_putchar(str[i]);
 _putchar('\n');
 }
