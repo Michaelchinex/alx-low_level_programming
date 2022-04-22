bcd25e6f176766fb627b9f96efab5409d95df708
Michaelchinex <okohagchidinma@gmail.com>
2022-04-22 02:54:28 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 /**
-* main - prime numbers
+*main - entry point
 *
-* Return: Always 0.
+*Return:0(success)
+*
+*Description:testing above function
+*
 */
 int main(void)
 {
-unsigned int long n = 612852475143, a = (int) sqrt(n);
-while (1)
+long int n, max, i;
+max = -1;
+n = 612852475143;
+while (n % 2 == 0)
 {
-if (n % a == 0)
+max = 2;
+n = n / 2;
+}
+for (i = 3; i <= n / 2; i = i + 2)
 {
-printf("%lu \n", n / a);
-break;
+while (n % i == 0)
+{
+max = i;
+n = n / i;
 }
-a--;
 }
+if (n > 2)
+{
+max = n;
+}
+printf("%ld\n", max);
 return (0);
 }
