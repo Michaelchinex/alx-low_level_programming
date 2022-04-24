6adfbcd24c68c25ff5870ee8c72b9443ac0eb432
Michaelchinex <okohagchidinma@gmail.com>
2022-04-23 17:35:40 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,10 +10,8 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-/* i = iterator for n1 and n2; j = iterator for r; n = carry over number */
 int i, j, n;
 i = j = n = 0;
-/* if r[0] >= 10, set value to 1 & increase buffer size by 1*/
 if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 {
 r[0] = 1 + '0';
@@ -27,7 +25,6 @@ else
 n = 0;
 r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
 r[j] = r[j] % 10 + '0';
-/* printf("i: %d, n1: %d, n2: %d, j: %d, r: %d\n", i, n1[i] - '0', n2[i] - '0', j, r[j]- '0'); debug*/
 i++;
 j++;
 if (n1[i] == '\0' || n2[i] == '\0')
